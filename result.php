f676334f3c6ac526d71947831da6f5cedad29b4f
drwolf72 <matts339@gmail.com>
2015-05-25 16:28:09 -0700

    matt

--- result.php
+++ result.php
@@ -49,6 +49,90 @@
 		{
 			echo "<br>";
 		}
+		if ((($birthmonth == "april") && ($birthday >="20")) || ($birthmonth == "may" && ($birthday >="1" && $birthday <="20")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Taurus</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "may") && ($birthday >="21")) || ($birthmonth == "june" && ($birthday >="1" && $birthday <="20")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Gemini</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "june") && ($birthday >="21")) || ($birthmonth == "july" && ($birthday >="1" && $birthday <="22")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Cancer</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "july") && ($birthday >="23")) || ($birthmonth == "august" && ($birthday >="1" && $birthday <="22")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Leo</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "august") && ($birthday >="23")) || ($birthmonth == "september" && ($birthday >="1" && $birthday <="22")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Virgo</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "september") && ($birthday >="23")) || ($birthmonth == "october" && ($birthday >="1" && $birthday <="22")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Libra</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "october") && ($birthday >="23")) || ($birthmonth == "november" && ($birthday >="1" && $birthday <="21")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Scorpio</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "november") && ($birthday >="22")) || ($birthmonth == "december" && ($birthday >="1" && $birthday <="21")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Sagittarius</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+		if ((($birthmonth == "december") && ($birthday >="22")) || ($birthmonth == "january" && ($birthday >="1" && $birthday <="19")))
+		{
+			echo "<br>";
+			echo "<h2>your astrological sign is Capricorn</h2>";
+		}
+		else
+		{
+			echo "<br>";
+		}
+
+
+
 
 		?>
 
